037954b45e4d2123acf0a94f4b1aefe35f61b579
Your Name <you@example.com>
2024-11-16 19:57:24 +0500

    first commit

---
+++ app/src/main/java/com/example/todoapp_roomdb/MainApplication.kt
@@ -0,0 +1,22 @@
+package com.example.todoapp_roomdb
+
+import android.app.Application
+import androidx.room.Room
+import com.example.todoapp_roomdb.db.TodoDatabase
+
+class MainApplication : Application() {
+
+    companion object {
+        lateinit var todoDatabase: TodoDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        todoDatabase = Room.databaseBuilder(
+            applicationContext,
+            TodoDatabase::class.java,
+            TodoDatabase.NAME
+        ).build()
+    }
+
+}
